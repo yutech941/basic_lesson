646de8320a4335b7c8cc57cdcca968319892288c
小林優一 <y.k94cofitec179@gmail.com>
2022-04-15 20:43:12 +0900

    cleanUpコンポーネント作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { Counter } from "./features/counter/Counter";
 import "./App.css";
+import CleanUp from "./CleanUp";
 
 const App: React.FC = () => {
   const [status, setStatus] = useState<string | number>("text");
   const [input, setInput] = useState("");
   const [counter, setCounter] = useState(0);
+  const [display, setDisplay] = useState(true);
 
   const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value);
@@ -25,6 +27,8 @@ const App: React.FC = () => {
         <button onClick={() => setCounter((preCounter) => preCounter + 1)}>
           Increment
         </button>
+        {display && <CleanUp />}
+        <button onClick={() => setDisplay(!display)}>Toggle display</button>
         <Counter />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
